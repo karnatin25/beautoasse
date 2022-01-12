a79d9614febaec13ff5fc0c4851675f2d5bb6b62
NagarjunaReddy <karnatin25@gmail.com>
2022-01-12 15:39:49 +0530

    something added

--- product/admin.py
+++ product/admin.py
@@ -3,5 +3,5 @@ from .models import Product_Beato
 # Register your models here.
 
 class Product_data(admin.ModelAdmin):
-    list_display=['name','description','cost_per_item','stock_quantity','volume','quantity']
+    list_display=['name','description','cost_per_item','stock_quantity','volume']
 admin.site.register(Product_Beato,Product_data)
--- product/models.py
+++ product/models.py
@@ -7,9 +7,12 @@ class Product_Beato(models.Model):
     description = models.TextField()
     cost_per_item = models.IntegerField()
     stock_quantity = models.IntegerField()
-    volume = models.IntegerField()
+    volume = models.IntegerField(blank= True)
 
-
+    def save(self, *args, **kwargs):
+        self.volume = self.cost_per_item * self.stock_quantity
+        super(Product_Beato, self).save(*args, **kwargs)
+        
     def quantity(self):
         if(self.cost_per_item != None):
             quan = (self.cost_per_item*self.stock_quantity)
