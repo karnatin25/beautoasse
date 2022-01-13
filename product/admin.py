7238b432d41f1f1305f6dfed9e48ea25c2458b85
NagarjunaReddy <karnatin25@gmail.com>
2022-01-13 12:47:19 +0530

    added test cases

--- product/admin.py
+++ product/admin.py
@@ -3,5 +3,5 @@ from .models import Product_Beato
 # Register your models here.
 
 class Product_data(admin.ModelAdmin):
-    list_display=['name','description','cost_per_item','stock_quantity','volume']
+    list_display=['name','description','cost_per_item','stock_quantity','volume','quantity']
 admin.site.register(Product_Beato,Product_data)
--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+# # # Create your tests here.
+
+
+
+from .models import Product_Beato
+
+class ProductTestCase(TestCase):
+    def setUp(self):
+        Product_Beato.objects.create(name="Dell", description="4 GB Ram and 1 TB hard disk", cost_per_item=35000 ,stock_quantity=10)
+
+    def test_product_value(self):
+
+        data = Product_Beato.objects.get(name="Dell")
+
+        self.assertEqual(data.volume,350000)
